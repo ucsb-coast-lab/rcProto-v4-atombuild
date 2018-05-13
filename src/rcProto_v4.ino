13ae7f914418098f56ed5bb4a318c0886eb10fac
Spencer Dickinson <scd@umail.ucsb.edu>
2018-05-13 16:29:00 -0700

    Changed WAITFORGPS to false

--- src/rcProto_v4.ino
+++ src/rcProto_v4.ino
@@ -68,7 +68,7 @@ TODO
 #define GPSECHO  false
 #define BBBECHO  false
 //#define XBECHO  false
-#define WAITFORGPS true
+#define WAITFORGPS false
 
 
 // ----------------------- Pins -----------------------
